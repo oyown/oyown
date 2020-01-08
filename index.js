018bc54f461dab6bcb541bd8110577d0f3e28cee
oyown <ayan7414@gmail.com>
2020-01-08 22:10:08 +0530

    select 2 added and initiated

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+    // REMOVE THIS LATER
     $(".to-do li").click(function(e){
         $(this).toggleClass("striked");
+    })
+
+    // Select 2
+    $("select").each(function(e){
+        $(this).select2({
+            minimumResultsForSearch: Infinity
+        });
     })
 })
